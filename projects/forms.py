7bc9ea1113aad97dc4951a048029d7dd0a3040cf
Muchori Joseph <muchorijoseph97@gmail.com>
2022-04-15 18:46:34 +0300

    all staged changes

---
+++ projects/forms.py
@@ -0,0 +1,41 @@
+from django.forms import ModelForm, widgets
+from django import forms
+from . models import Project, Review
+
+class ProjectForm(ModelForm):
+    class Meta:
+        model = Project
+        fields = ['title', 'featured_image', 'description',
+                  'demo_link', 'source_link']
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
+
+        #styling each fields individually
+        
+        # self.fields['title'].widget.attrs.update({'class':'input', 'placeholder':'Add Title'})
+        # self.fields['description'].widget.attrs.update({'class':'input', 'placeholder':'Add Description'})
+        # self.fields['demo_link'].widget.attrs.update({'class':'input', 'placeholder':'Add demo link'})
+        # self.fields['source_link'].widget.attrs.update({'class':'input', 'placeholder':'Add source link'})
+
+class ReviewForm(ModelForm):
+    class Meta:
+        model = Review
+        fields = ['value', 'body']
+        labels = {
+            'value':'Place your vote',
+            'body': 'Add comment with your vote'
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ReviewForm, self).__init__(*args, **kwargs)
+
+        #styling fields 
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
